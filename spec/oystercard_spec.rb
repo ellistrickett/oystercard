0289af582ca7b571ad7a6f639ec1fc25035a9d43
Ellis Trickett <ellis.trickett@gmail.com>
2020-04-20 20:49:32 +0100

    Challenge 5 complete: added top up method'

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe Oystercard do
   it 'has a default balance of 0' do 
     expect(subject.balance).to eq 0
   end
+
+  it { is_expected.to respond_to(:top_up).with(1).argument }
+
+  it 'tops up card by amount given' do 
+    subject.top_up(10)
+    expect(subject.balance).to eq 10
+  end
 end
